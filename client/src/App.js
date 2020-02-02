b0d781477b5792efb2e4812bfb2c2b00228364d8
Dan <dan@dan.com>
2020-02-02 18:33:28 +0200

    Initial commit. Set up of the project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Join from './components/Join'
+import Chat from './components/Chat'
+
+const App = () => (
+	<Router>
+		<Route exact path="/" component={Join} />
+		<Route exact path="/" component={Chat} />
+	</Router>
+)
+
+
